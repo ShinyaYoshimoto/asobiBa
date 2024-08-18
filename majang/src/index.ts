8b6329a9b5e81515c22daf9bb1219f2e2fcf60c4
ysmt <shinya.yoshimoto.67@gmail.com>
2024-08-18 16:37:55 +0900

    feat(majang): :tada: 符と飜数から、点数を返すAPIを作る

--- majang/src/index.ts
+++ majang/src/index.ts
@@ -9,6 +9,16 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.status(200).send(JSON.stringify({status: 'ok'}));
 });
 
+/**
+ * 符数と翻数を受け取り、点数を計算する
+ * @param req.body.symbolCount 符数
+ * @param req.body.fanCount 翻数
+ */
+app.post('/calculate-score', (req: express.Request, res: express.Response) => {
+  const {symbolCount, fanCount} = req.body;
+  res.status(200).send(JSON.stringify({status: 'ok', response: {score: 1000}}));
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Start on port 3000.');
 });
