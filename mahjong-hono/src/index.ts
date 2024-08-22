e9f347a9ab459e06976d5a84a7ee9a0a909a642e
ysmt <shinya.yoshimoto.67@gmail.com>
2024-08-22 23:58:27 +0900

    feat(mahjong hono): :sparkles: 点数計算用のエンドポイント作成

--- mahjong-hono/src/index.ts
+++ mahjong-hono/src/index.ts
@@ -1,6 +1,8 @@
 import {serve} from '@hono/node-server';
-import { swaggerUI } from '@hono/swagger-ui';
-import { OpenAPIHono } from '@hono/zod-openapi';
+import {swaggerUI} from '@hono/swagger-ui';
+import {OpenAPIHono} from '@hono/zod-openapi';
+import {createRoute} from '@hono/zod-openapi';
+import {z} from 'zod';
 
 const app = new OpenAPIHono();
 
@@ -14,9 +16,45 @@ app.doc31('doc', {
 
 app.get('/ui', swaggerUI({url: '/doc'}));
 
+// health check
 app.get('/', c => {
   return c.text('Hello Hono!');
 });
+
+app.openapi(
+  createRoute({
+    method: 'post',
+    path: '/scores/calculate',
+    request: {
+      body: {
+        content: {
+          "application/json": {
+            schema: z.object({
+              symbolCount: z.number(),
+              fanCount: z.number(),
+            }),
+          }
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: 'OK',
+        content: {
+          "application/json": {
+            schema: z.object({
+              score: z.number(),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    const {symbolCount, fanCount} = await c.req.json();
+    return c.json({score: symbolCount * fanCount});
+  },
+);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
