e27ade6ab9b930b2ad58e47920fcd5a170beba56
ysmt <shinya.yoshimoto.67@gmail.com>
2024-08-29 22:46:29 +0900

    feat(mahjong hono): 点数回答APIのI/Fを定義

---
+++ mahjong-hono/src/api/scores/answer/schema.ts
@@ -0,0 +1,25 @@
+import {z} from 'zod';
+
+export const requestBodySchema = z.object({
+  question: z.object({
+    isStartPlayer: z.boolean(),
+    isDraw: z.boolean(),
+    symbolCount: z.number().min(20).max(100).optional(),
+    fanCount: z.number().min(1),
+  }),
+  answer: z.object({
+    score: z.object({
+      startPlayer: z.number(),
+      other: z.number(),
+    }),
+  }),
+});
+
+export const responseBodySchema = z.object({
+  isCorrect: z.boolean(),
+});
+
+// FIXME: 共通化してもいいはず
+export const errorResponseBodySchema = z.object({
+  message: z.string(),
+});
