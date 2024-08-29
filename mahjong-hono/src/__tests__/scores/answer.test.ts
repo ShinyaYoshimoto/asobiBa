eedc056140b307cd30616cb2e81d322ea69cf6da
ysmt <shinya.yoshimoto.67@gmail.com>
2024-08-30 00:15:19 +0900

    test(mahjong hono): 正常系テストの修正＆追加、異常系テストの追加

--- mahjong-hono/src/__tests__/scores/answer.test.ts
+++ mahjong-hono/src/__tests__/scores/answer.test.ts
@@ -1,33 +1,94 @@
 import app from '../../app';
 
 describe('scores/answer', () => {
-  it('子の30符1翻ツモは、子から300点、親から500点のあがりである', async () => {
-    // Arrange
-    const question = {
-      isStartPlayer: false,
-      isDraw: true,
-      symbolCount: 30,
-      fanCount: 1,
-    };
-    const answer = {
-      score: {
-        startPlayer: 300,
-        other: 500,
-      },
-    };
+  describe('正常系', () => {
+    it('子の30符1翻ツモは、子から300点、親から500点のあがりである', async () => {
+      // Arrange
+      const question = {
+        isStartPlayer: false,
+        isDraw: true,
+        symbolCount: 30,
+        fanCount: 1,
+      };
+      const answer = {
+        score: {
+          startPlayer: 500,
+          other: 300,
+        },
+      };
 
-    // Action
-    const response = await app.request('/scores/answer', {
-      method: 'POST',
-      body: JSON.stringify({
-        question,
-        answer,
-      }),
+      // Action
+      const response = await app.request('/scores/answer', {
+        method: 'POST',
+        body: JSON.stringify({
+          question,
+          answer,
+        }),
+      });
+
+      const result = await response.json();
+
+      // Assert
+      expect(result.isCorrect).toBeTruthy();
     });
 
-    const result = await response.json();
+    it('子の30符1翻ツモは、子から300点、親から300点のあがりである', async () => {
+      // Arrange
+      const question = {
+        isStartPlayer: false,
+        isDraw: true,
+        symbolCount: 30,
+        fanCount: 1,
+      };
+      const answer = {
+        score: {
+          startPlayer: 300,
+          other: 300,
+        },
+      };
 
-    // Assert
-    expect(result.isCorrect).toBeTruthy();
+      // Action
+      const response = await app.request('/scores/answer', {
+        method: 'POST',
+        body: JSON.stringify({
+          question,
+          answer,
+        }),
+      });
+
+      const result = await response.json();
+
+      // Assert
+      expect(result.isCorrect).toBeFalsy();
+    });
+  });
+  describe('異常系', () => {
+    it('子の0符1翻ツモは、子から300点、親から500点のあがりである', async () => {
+      // Arrange
+      const question = {
+        isStartPlayer: false,
+        isDraw: true,
+        symbolCount: 0,
+        fanCount: 1,
+      };
+      const answer = {
+        score: {
+          startPlayer: 300,
+          other: 500,
+        },
+      };
+
+      // Action
+      const response = await app.request('/scores/answer', {
+        method: 'POST',
+        body: JSON.stringify({
+          question,
+          answer,
+        }),
+      });
+
+      // Assert
+      expect(response.status).toBe(400);
+    });
   });
 });
