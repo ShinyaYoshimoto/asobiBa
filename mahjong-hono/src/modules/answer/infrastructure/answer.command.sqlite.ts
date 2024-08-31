3f5be2e70a6b107f3b22e806d2f054c27c1fce13
ysmt <shinya.yoshimoto.67@gmail.com>
2024-08-31 16:52:42 +0900

    feat(mahjong hono): RDBに登録するI/Fと実装

---
+++ mahjong-hono/src/modules/answer/infrastructure/answer.command.sqlite.ts
@@ -0,0 +1,21 @@
+import {AnswerCommandInterface} from '../domain/answer.command';
+import {AnswerEntity, AnswerSchema} from '../domain/answer.entity';
+import {PrismaClient} from '@prisma/client';
+
+export class AnswerCommandSqlite implements AnswerCommandInterface {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async register(answer: AnswerEntity): Promise<AnswerEntity> {
+    const createdAnswer = await this.prisma.answer.create({
+      data: {
+        isStartPlayer: answer.isStartPlayer(),
+        isDraw: answer.isDraw(),
+        fanCount: answer.fanCount(),
+        symbolCount: answer.symbolCount(),
+        isCorrect: answer.isCorrect(),
+      },
+    });
+
+    return AnswerEntity.create(AnswerSchema.parse(createdAnswer));
+  }
+}
