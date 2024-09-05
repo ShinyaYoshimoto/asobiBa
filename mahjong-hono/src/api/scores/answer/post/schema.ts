ab5a912b0568737d0ffaa48f6f6528f40f1aceef
ysmt <shinya.yoshimoto.67@gmail.com>
2024-09-06 07:41:54 +0900

    style(mahjong hono): schemaにopenapi項目を付与

--- mahjong-hono/src/api/scores/answer/post/schema.ts
+++ mahjong-hono/src/api/scores/answer/post/schema.ts
@@ -2,15 +2,20 @@ import {z} from 'zod';
 
 export const requestBodySchema = z.object({
   question: z.object({
-    isStartPlayer: z.boolean({description: '親である'}),
-    isDraw: z.boolean({description: 'ツモである'}),
-    symbolCount: z.number({description: '符数'}).min(20, '符数は20以上です').max(110, '符数は110以下です').optional(),
-    fanCount: z.number({description: '翻数'}).min(1, '翻数は1以上です'),
+    isStartPlayer: z.boolean({description: '親である'}).openapi({default: false}),
+    isDraw: z.boolean({description: 'ツモである'}).openapi({default: true}),
+    symbolCount: z
+      .number({description: '符数'})
+      .min(20, '符数は20以上です')
+      .max(110, '符数は110以下です')
+      .optional()
+      .openapi({default: 30}),
+    fanCount: z.number({description: '翻数'}).min(1, '翻数は1以上です').openapi({default: 1}),
   }),
   answer: z.object({
     score: z.object({
-      startPlayer: z.number({description: '親の支払い. 支払いがない場合は0を指定してください'}),
-      other: z.number({description: '子の支払い. 支払いがない場合は0を指定してください'}),
+      startPlayer: z.number({description: '親の支払い. 支払いがない場合は0を指定してください'}).openapi({default: 500}),
+      other: z.number({description: '子の支払い. 支払いがない場合は0を指定してください'}).openapi({default: 300}),
     }),
   }),
 });
