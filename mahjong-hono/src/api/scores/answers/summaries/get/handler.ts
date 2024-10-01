de7b0604ab1f7d6df6c4b15f01d241e8a2b4a760
ysmt <shinya.yoshimoto.67@gmail.com>
2024-10-02 07:18:20 +0900

    feat(mahjong hono): テスト追加

--- mahjong-hono/src/api/scores/answers/summaries/get/handler.ts
+++ mahjong-hono/src/api/scores/answers/summaries/get/handler.ts
@@ -2,14 +2,17 @@ import {Context} from 'hono';
 import {AnswerQuerySqlite} from '../../../../../modules/answer/infrastructure/answer.query.sqlite';
 import {PrismaClient} from '@prisma/client';
 import {AnswerQueryInterface} from '../../../../../modules/answer/domain/answer.query';
+import {basicLogger, loggerInterface} from '../../../../../utils/logger';
 
 export class ScoresAnswersSummariesHandler {
   private readonly answerQuery: AnswerQueryInterface;
   private readonly prismaClient: PrismaClient;
+  private readonly logger: loggerInterface;
 
-  constructor(dep?: {answerQuery?: AnswerQueryInterface}) {
+  constructor(dep?: {answerQuery?: AnswerQueryInterface, logger?: loggerInterface}) {
     this.prismaClient = new PrismaClient();
     this.answerQuery = dep?.answerQuery ?? new AnswerQuerySqlite(this.prismaClient);
+    this.logger = dep?.logger ?? new basicLogger();
   }
 
   handle = async (c: Context) => {
@@ -17,7 +20,7 @@ export class ScoresAnswersSummariesHandler {
       const answers = await this.answerQuery.loadSummary();
       return c.json({answers: answers.map(answer => ({...answer, count: answer.count}))}, 200);
     } catch (e) {
-      // TODO: logging
+      this.logger.error('Internal Server Error', e);
       return c.json({message: 'Internal Server Error'}, 500);
     }
   };
