5e44681f6ee6b307b3eae39d4e79752ed497e8a7
ysmt <shinya.yoshimoto.67@gmail.com>
2024-10-29 09:13:49 +0900

    test(mahjong hono): まずはハッピーパターンのテストを書く

--- mahjong-hono/src/api/hands/get/handler.test.ts
+++ mahjong-hono/src/api/hands/get/handler.test.ts
@@ -3,19 +3,28 @@ import app from '../../../app';
 describe('GET /hands', () => {
   beforeEach(() => {
     // TODO: モッククリア
-  })
+  });
 
   describe('正常系', () => {
     it('happy path', async () => {
       // Arrange
-    // TODO: モックの作成
-    // ただ、データ自体は初期投入データを取得するだけなので、モックは不要かも
+      // TODO: モックの作成
+      // ただ、データ自体は初期投入データを取得するだけなので、モックは不要かも
 
-    // Action
-    const response = await app.request('/hands');
+      // Action
+      const response = await app.request('/hands');
+      const result = await response.json();
 
-    // Assert
+      // Assert
       expect(response.status).toBe(200);
+      expect(result.hands.length).toBeGreaterThan(0);
+      expect(result.hands[0]).toBe(expect.objectContaining({
+        id: expect.any(String),
+        name: expect.any(String),
+        nameKana: expect.any(String),
+        fanCount: expect.any(Number),
+        fanCountForCall: expect.any(Number),
+      }));
     });
   });
 });
