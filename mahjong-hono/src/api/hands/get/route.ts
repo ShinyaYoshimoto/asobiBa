a10f21518f9e73831e3e21c7f7eb57111106b4e3
ysmt <shinya.yoshimoto.67@gmail.com>
2024-11-01 07:16:14 +0900

    docs(mahjong hono): API定義の肉付け

--- mahjong-hono/src/api/hands/get/route.ts
+++ mahjong-hono/src/api/hands/get/route.ts
@@ -5,6 +5,13 @@ import {errorResponseSchema} from '../../common/schema';
 export const handsRoute = createRoute({
   method: 'get',
   path: '/hands',
+  tags: ['hands'],
+  summary: '有効な全ての役を取得します',
+  description:
+    `## 概要\n\n` +
+    `- Mリーグで認められているアガリ役の一覧を取得します\n\n` +
+    `## 注意\n\n` +
+    `- 門前役で鳴きありの場合には、役が付かない為、0を返します`,
   responses: {
     200: {
       description: 'Success',
--- mahjong-hono/src/api/hands/get/schema.ts
+++ mahjong-hono/src/api/hands/get/schema.ts
@@ -3,11 +3,11 @@ import {z} from 'zod';
 export const responseBodySchema = z.object({
   hands: z.array(
     z.object({
-      id: z.string(),
-      name: z.string(),
-      nameKana: z.string(),
-      fanCount: z.number(),
-      fanCountForCall: z.number(),
+      id: z.string().openapi({description: '役ID'}),
+      name: z.string().openapi({description: '役名'}),
+      nameKana: z.string().openapi({description: '役名カナ'}),
+      fanCount: z.number().openapi({description: '翻数'}),
+      fanCountForCall: z.number().openapi({description: '鳴きあり時の翻数'}),
     })
   ),
 });
